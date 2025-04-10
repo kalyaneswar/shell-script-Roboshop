eeeeb7e67888ea1013bfaa8ba17f4f7d7be8f969
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2025-04-10 11:19:25 +0530

    shell roboshop rabbitMQ

--- 04-rabbitmq.sh
+++ 04-rabbitmq.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 USERID=$(id -u)
-TIMESTAMP=$(date +%F)
+TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-
-# Color codes for terminal output
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-# Function to validate the success of commands
-VALIDATE() {
-    if [ $1 -ne 0 ]; then
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
         echo -e "$2...$R FAILURE $N"
         exit 1
     else
@@ -21,31 +19,36 @@ VALIDATE() {
     fi
 }
 
-# Ensure the script is being run as root
-if [ $USERID -ne 0 ]; then
+if [ $USERID -ne 0 ]
+then
     echo "Please run this script with root access."
-    exit 1
+    exit 1 # manually exit if error comes.
 else
     echo "You are super user."
 fi
 
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>$LOGFILE
-VALIDATE $? ""Erlang script installation"
+VALIDATE $? "Erlang script installation"
 
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>$LOGFILE
-VALIDATE $? ""Server script installation"
+VALIDATE $? "Server script installation"
 
 dnf install rabbitmq-server -y &>>$LOGFILE
 VALIDATE $? "Installing RabbitMQ"
 
-systemctl enable rabbitmq-server &>>$LOGFILE
+systemctl enable rabbitmq-server  &>>$LOGFILE
 VALIDATE $? "Enabling RabbitMQ"
 
-systemctl start rabbitmq-server &>>$LOGFILE
+systemctl start rabbitmq-server  &>>$LOGFILE
 VALIDATE $? "Starting RabbitMQ"
 
-# rabbitmqctl add_user roboshop roboshop123 &>>$LOGFILE
-# VALIDATE $? "Adding user in RabbitMQ "
-
-# rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOGFILE
-# VALIDATE $? "Setting permissions RabbitMQ "
+sudo rabbitmqctl list_users | grep roboshop &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    rabbitmqctl add_user roboshop roboshop123 &>>$LOGFILE
+    VALIDATE $? "Adding RabbitMQ user"
+    rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOGFILE
+    VALIDATE $? "Setting permissions"
+else
+    echo -e "USER already exists... $Y SKIPPING $N"
+fi
